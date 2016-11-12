27332dde32ee6d6fc1ebdee5f6544d34b05ef209
samgalson@gmail.com <samgalson@gmail.com>
2016-11-12 17:49:07 +0000

    feat: scores next moves looking one move ahead

--- src/ai.js
+++ src/ai.js
@@ -1,9 +1,11 @@
 import * as fromBoard from './board';
 
-export const score = (board, player) =>
+export const score = player => board =>
   (fromBoard.isWinner(board, player) ? 1 : 0);
 
 export const getNextBoards = (board, player) =>
     fromBoard.getEmptyIndices(board, player)
     .map(fromBoard.movePlayerToIndex(board, player));
+
+export const scoreNextMoves = (board, player) => getNextBoards(board, player).map(score(player));
 
--- test/ai.test.js
+++ test/ai.test.js
@@ -2,16 +2,16 @@ import test from 'ava';
 import * as ai from '../src/ai';
 
 test('scores a board for a player', (t) => {
-  t.is(ai.score([
+  t.is(ai.score(1)([
     1, 0, 0,
     2, 1, 2,
     2, 0, 1,
-  ], 1), 1);
-  t.is(ai.score([
+  ]), 1);
+  t.is(ai.score(2)([
     1, 0, 0,
     2, 1, 2,
     2, 0, 1,
-  ], 2), 0);
+  ]), 0);
 });
 
 test('generates next possible boards', (t) => {
@@ -36,3 +36,16 @@ test('generates next possible boards', (t) => {
   ], 2), expected);
 });
 
+test('scores moves looking one move ahead', (t) => {
+  t.deepEqual(ai.scoreNextMoves([
+    1, 1, 0,
+    2, 1, 2,
+    2, 0, 0,
+  ], 2), [0, 0, 0]);
+
+  t.deepEqual(ai.scoreNextMoves([
+    1, 1, 0,
+    2, 1, 0,
+    2, 0, 0,
+  ], 1), [1, 0, 1, 1]);
+});
