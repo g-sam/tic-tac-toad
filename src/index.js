91a36af5dd50984f9a63dfac2db7074bf7c4dbd6
samgalson@gmail.com <samgalson@gmail.com>
2016-11-23 20:29:57 +0000

    refactor: updates index to work with new structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+/* global window */
 import $ from 'jquery';
 import DOMRenderer from './dom';
 import Controller from './controller';
+import UI from './ui/';
+import Game from './ui/game';
+import Opts from './ui/options';
+import Logic from './logic/';
 
-const dom = new DOMRenderer($);
-const controller = new Controller(dom);
-controller.execute();
+const logic = new Logic();
+const game = new Game(logic);
+const opts = new Opts();
+const ui = new UI(game, opts);
+const dom = new DOMRenderer(window, $);
+const controller = new Controller(dom, ui);
+
+controller.render();
